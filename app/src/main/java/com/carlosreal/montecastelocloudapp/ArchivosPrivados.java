b4ee446ad5b7dfb4c353e06b197d35c6b0119466
carlos <crealgar00@gmail.com>
2022-11-22 00:56:28 +0100

    UPDATE: InfoArchivo and Menu

--- app/src/main/java/com/carlosreal/montecastelocloudapp/ArchivosPrivados.java
+++ app/src/main/java/com/carlosreal/montecastelocloudapp/ArchivosPrivados.java
@@ -1,10 +1,13 @@
 package com.carlosreal.montecastelocloudapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -21,13 +24,14 @@ public class ArchivosPrivados extends AppCompatActivity {
     public static ArrayList<ArchivoPrivado> ListaPrivate = new ArrayList<>();
     private Button AddArchivoPriv;
     Connection Conn;
+    int Usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivos_privados);
         ListaPriv = findViewById(R.id.ListaPrivado);
-        int Usuario = this.getIntent().getExtras().getInt("Id");
+        Usuario = this.getIntent().getExtras().getInt("Id");
         try{
             ConnectionHelper ConnHelp = new ConnectionHelper();
             Conn = ConnHelp.ConnectionClass();
@@ -53,6 +57,29 @@ public class ArchivosPrivados extends AppCompatActivity {
         }
         catch (Exception ex){
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.MnuOpc1:
+                Intent Intent = new Intent(this, ArchivosPrivados.class);
+                Intent.putExtra("Id", Usuario);
+                startActivity(Intent);
+                return true;
+            case R.id.MnuOpc2:
+                Intent Intent1 = new Intent(this, ArchivosPublicos.class);
+                Intent1.putExtra("Id", Usuario);
+                startActivity(Intent1);
+                return true;
+            case R.id.MnuOpc3:
+                Intent Intent2 = new Intent(this, Login.class);
+                Intent2.putExtra("Id", Usuario);
+                startActivity(Intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
